bca39958557398ba2321049a5aadab97073fa21b
Vincent Lin <vinlin24@outlook.com>
2024-04-09 17:06:51 -0700

    Update nicknames to preferred names in addition

--- src/commands/moderation/assign-inductees.ts
+++ src/commands/moderation/assign-inductees.ts
@@ -16,17 +16,22 @@ import {
 import { makeErrorEmbed } from "../../utils/errors.utils";
 import { INDUCTEES_ROLE_ID } from "../../utils/snowflakes.utils";
 
-module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("assigninductees")
-    .setDescription("Assign the Inductees role to all registered inductees.")
-    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
-  execute: assignInducteesRole,
-};
+const COMMAND_NAME = "assigninductees";
 
 const INDUCTEE_INFO_CSV_PATH = "inductees.csv"; // Placed at CWD for now.
+const FIRST_NAME_COL_NAME = "Preferred First Name";
+const LAST_NAME_COL_NAME = "Preferred Last Name";
+const DISCORD_USERNAME_COL_NAME = "Discord Username";
 
-async function assignInducteesRole(
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName(COMMAND_NAME)
+    .setDescription("Update all server members that are registered inductees.")
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+  execute: updateInducteeMembers,
+};
+
+async function updateInducteeMembers(
   interaction: ChatInputCommandInteraction,
 ): Promise<void> {
   const { guild } = interaction;
@@ -42,37 +47,56 @@ async function assignInducteesRole(
   const role = guild.roles.cache.get(INDUCTEES_ROLE_ID);
   if (!role) {
     await interaction.reply({
+      ephemeral: true,
       embeds: [makeErrorEmbed(
         "Could not find the inductee role " +
         `(expected role with ID ${inlineCode(INDUCTEES_ROLE_ID)}).`,
       )],
-      ephemeral: true,
     });
     return;
   }
 
-  const usernames = parseInducteeUsernamesFromCSV();
-  if (usernames === "File Not Found") {
+  const inducteesInfo = parseInducteeInfoFromCSV();
+  if (inducteesInfo === "File Not Found") {
     await interaction.reply({
+      ephemeral: true,
       embeds: [makeErrorEmbed(
         "Could not find the file with inductee information!",
       )],
-      ephemeral: true,
     });
     return;
   }
+  if (inducteesInfo === "File Malformed") {
+    await interaction.reply({
+      ephemeral: true,
+      embeds: [makeErrorEmbed(
+        "Inductee info file has an unexpected format!",
+      )],
+    })
+    return;
+  }
 
-  // Not sure how long this takes with many inductees, so just in case.
+  // Not sure how long updating takes with many inductees, so just in case.
   await interaction.deferReply();
 
-  const [members, notFound] = await getMembersFromUsernames(usernames, guild);
-  const [succeeded, failed] = await assignInducteesRoleToMembers(members);
+  const [
+    succeeded,
+    notFound,
+    failed,
+  ] = await findAndUpdateMembersWithInfo(inducteesInfo, guild);
 
   const embed = prepareResponseEmbed(succeeded, notFound, failed, role);
   await interaction.editReply({ embeds: [embed] });
 }
 
-function parseInducteeUsernamesFromCSV(): string[] | "File Not Found" {
+type InducteeInfo = {
+  firstName: string;
+  lastName: string;
+  discordUsername: string;
+};
+
+function parseInducteeInfoFromCSV():
+  InducteeInfo[] | "File Not Found" | "File Malformed" {
   if (!fs.existsSync(INDUCTEE_INFO_CSV_PATH)) {
     console.error(`ERROR: file at path ${INDUCTEE_INFO_CSV_PATH} not found.`);
     return "File Not Found";
@@ -86,82 +110,137 @@ function parseInducteeUsernamesFromCSV(): string[] | "File Not Found" {
     return [];
   }
 
-  const usernameColumnIndex = header.indexOf("Discord Username");
-  const usernames = rows.map(row => row[usernameColumnIndex]);
-  return usernames;
+  const firstNameColumnIndex = header.indexOf(FIRST_NAME_COL_NAME);
+  if (firstNameColumnIndex === -1) {
+    console.error(`ERROR: no '${FIRST_NAME_COL_NAME}' column.`);
+    return "File Malformed";
+  }
+
+  const lastNameColumnIndex = header.indexOf(LAST_NAME_COL_NAME);
+  if (lastNameColumnIndex === -1) {
+    console.error(`ERROR: no '${LAST_NAME_COL_NAME}' column.`);
+    return "File Malformed";
+  }
+
+  const usernameColumnIndex = header.indexOf(DISCORD_USERNAME_COL_NAME);
+  if (usernameColumnIndex === -1) {
+    console.error(`ERROR: no '${DISCORD_USERNAME_COL_NAME}' column`);
+    return "File Malformed";
+  }
+
+  const inducteesInfo: InducteeInfo[] = rows.map(row => ({
+    firstName: row[firstNameColumnIndex],
+    lastName: row[lastNameColumnIndex],
+    discordUsername: row[usernameColumnIndex],
+  }));
+
+  return inducteesInfo;
 }
 
-async function getMembersFromUsernames(
-  usernames: string[],
+async function findAndUpdateMembersWithInfo(
+  inducteesInfo: InducteeInfo[],
   guild: Guild,
 ): Promise<[
-  foundMembers: GuildMember[],
-  notFoundUsernames: string[],
+  succeededMembers: GuildMember[],
+  notFoundUsers: InducteeInfo[],
+  failedMembers: GuildMember[]
 ]> {
-  const foundMembers: GuildMember[] = [];
-  const notFoundUsernames: string[] = [];
+  const succeededMembers: GuildMember[] = [];
+  const notFoundUsers: InducteeInfo[] = [];
+  const failedMembers: GuildMember[] = [];
 
-  for (const username of usernames) {
-    const members = await guild.members.fetch({ query: username, limit: 1 });
+  for (const inducteeInfo of inducteesInfo) {
+    const {
+      firstName,
+      lastName,
+      discordUsername: providedUsername,
+    } = inducteeInfo;
+
+    // Make the API call.
+    const members = await guild.members.fetch({
+      query: providedUsername,
+      limit: 1,
+    });
 
     // Unpack the singular member.
     const [member] = members.values();
 
     // Query returned no results or the username doesn't match for some reason.
-    if (!member || member.user.username !== username) {
-      notFoundUsernames.push(username);
+    if (!member || member.user.username !== providedUsername) {
+      notFoundUsers.push(inducteeInfo);
       continue;
     }
 
-    foundMembers.push(member);
-  }
+    // Do the actual updating.
+    const nickname = `${firstName} ${lastName}`;
+    const success =
+      await assignInducteeRole(member) &&
+      await updateMemberNickname(member, nickname);
 
-  return [foundMembers, notFoundUsernames];
-}
-
-async function assignInducteesRoleToMembers(
-  members: GuildMember[],
-): Promise<[
-  succeededMembers: GuildMember[],
-  failedMembers: GuildMember[],
-]> {
-  const succeededMembers: GuildMember[] = [];
-  const failedMembers: GuildMember[] = [];
-
-  for (const member of members) {
-    try {
-      await member.roles.add(INDUCTEES_ROLE_ID);
+    if (success) {
       succeededMembers.push(member);
-    } catch (error) {
-      const { code, message } = error as DiscordAPIError;
-      console.error(
-        `FAILED to assign Inductees role to @${member.user.username}: ` +
-        `DiscordAPIError[${code}] ${message}`,
-      );
+    } else {
       failedMembers.push(member);
     }
   }
 
-  return [succeededMembers, failedMembers];
+  return [succeededMembers, notFoundUsers, failedMembers];
+}
+
+async function assignInducteeRole(member: GuildMember): Promise<boolean> {
+  try {
+    await member.roles.add(INDUCTEES_ROLE_ID);
+    return true;
+  } catch (error) {
+    const { code, message } = error as DiscordAPIError;
+    console.error(
+      `FAILED to assign Inductees role to @${member.user.username}: ` +
+      `DiscordAPIError[${code}] ${message}`,
+    );
+    return false;
+  }
+}
+
+async function updateMemberNickname(
+  member: GuildMember,
+  nickname: string,
+): Promise<boolean> {
+  try {
+    await member.setNickname(
+      nickname,
+      `/${COMMAND_NAME}: used inductee's provided preferred name`,
+    );
+    return true;
+  } catch (error) {
+    const { code, message } = error as DiscordAPIError;
+    console.error(
+      `FAILED to update @${member.user.username} nickname to '${nickname}': ` +
+      `DiscordAPIError[${code}] ${message}`,
+    );
+    return false;
+  }
 }
 
 function prepareResponseEmbed(
   succeeded: GuildMember[],
-  notFound: string[],
+  notFound: InducteeInfo[],
   failed: GuildMember[],
   role: Role,
 ): EmbedBuilder {
   const successString = succeeded.length > 0 ? (
-    `✅ Assigned ${role} to ${succeeded.length} member(s)!`
+    `✅ Assigned ${role} and updated nickname for ${succeeded.length} members!`
   ) : "";
 
   const notFoundString = notFound.length > 0 ? (
     "⚠️ It doesn't seem like these users are in the server:\n" +
-    notFound.map(username => inlineCode(`@${username}`)).join(", ")
+    notFound.map(info => {
+      const { firstName, lastName, discordUsername: username } = info;
+      return inlineCode(`@${username}`) + " " + `(${firstName} ${lastName})`;
+    }).join(", ")
   ) : "";
 
   const failedString = failed.length > 0 ? (
-    "🚨 An error occurred when trying to give these members the role:\n" +
+    "🚨 I wasn't allowed to update these members:\n" +
     failed.join(", ")
   ) : "";
 
