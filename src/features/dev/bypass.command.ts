ae5071b0cbc4be3ddd5665aacd486cdd0f0b3e3b
Vincent Lin <vinlin24@outlook.com>
2025-04-03 09:29:26 -0700

    feat: add option to dev bypass ALL commands

--- src/features/dev/bypass.command.ts
+++ src/features/dev/bypass.command.ts
@@ -20,15 +20,14 @@ class BypassCommand extends SlashCommandHandler {
   public override readonly definition = new SlashCommandBuilder()
     .setName("devbypass")
     .setDescription("Enable/disable developer bypass for a command.")
-    .addStringOption(input => input
-      .setName("command_name")
-      .setDescription("Name of command to set bypass state.")
-      .setRequired(true),
-    )
     .addBooleanOption(input => input
       .setName("enable")
       .setDescription("True for enable, false for disable.")
       .setRequired(true)
+    )
+    .addStringOption(input => input
+      .setName("command_name")
+      .setDescription("Name of command to set bypass state (omit for all).")
     )
     .toJSON();
 
@@ -39,8 +38,16 @@ class BypassCommand extends SlashCommandHandler {
   public override async execute(
     interaction: ChatInputCommandInteraction,
   ): Promise<void> {
-    let commandName = interaction.options.getString("command_name", true);
+    let commandName = interaction.options.getString("command_name");
     const enable = interaction.options.getBoolean("enable", true);
+
+    if (commandName === null) {
+      await interaction.reply({
+        embeds: [this.processBypassAllOption(interaction, enable)],
+        ephemeral: true,
+      })
+      return;
+    }
 
     if (commandName.startsWith("/")) {
       commandName = commandName.slice(1);
@@ -71,6 +78,26 @@ class BypassCommand extends SlashCommandHandler {
       embeds: [successEmbed],
       ephemeral: true,
     });
+  }
+
+  private processBypassAllOption(
+    interaction: ChatInputCommandInteraction,
+    enable: boolean,
+  ): EmbedBuilder {
+    for (const handler of commandLoader.getAll().values()) {
+      handler.setDevBypass(enable);
+    }
+    console.warn(
+      `${formatContext(interaction)}: ${enable ? "enabled" : "disabled"} ` +
+      "developer bypass for ALL commands.",
+    );
+
+    const description = codeBlock("diff", enable ? "+ Enabled" : "- Disabled");
+    const successEmbed = new EmbedBuilder()
+      .setTitle("Developer Bypass: ALL Commands")
+      .setDescription(description)
+      .setColor(enable ? Colors.Green : Colors.Red);
+    return successEmbed;
   }
 }
 
