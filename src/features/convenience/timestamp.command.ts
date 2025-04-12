985bb49fd378d06de42d7e61fcf8bafcfd7cfd95
Vincent Lin <vinlin24@outlook.com>
2025-04-12 03:16:59 -0700

    feat: add help option to /timestamp

--- src/features/convenience/timestamp.command.ts
+++ src/features/convenience/timestamp.command.ts
@@ -1,5 +1,10 @@
 import {
+  bold,
   codeBlock,
+  Colors,
+  EmbedBuilder,
+  inlineCode,
+  italic,
   SlashCommandBuilder,
   time,
   TimestampStyles,
@@ -10,7 +15,7 @@ import {
 import type { DateTime } from "luxon";
 
 import { SlashCommandHandler } from "../../abc/command.abc";
-import type { UnixSeconds } from "../../types/branded.types";
+import type { UnixSeconds, UrlString } from "../../types/branded.types";
 import {
   Month,
   MONTH_NAMES,
@@ -18,6 +23,7 @@ import {
   type IDateClient,
 } from "../../utils/date.utils";
 import { makeErrorEmbed } from "../../utils/errors.utils";
+import { quietHyperlink, toBulletedList } from "../../utils/formatting.utils";
 
 const MONTH_CHOICES: APIApplicationCommandOptionChoice<Month>[] = MONTH_NAMES
   .map(name => ({ name: `${name} (${Month[name]})`, value: Month[name] }));
@@ -60,8 +66,12 @@ class TimestampCommand extends SlashCommandHandler {
   public override readonly definition = new SlashCommandBuilder()
     .setName("timestamp")
     .setDescription(
-      "Format a timestamp mention based on input date/time in UCLA time " +
+      "Format a timestamp mention based on input date/time, in UCLA time " +
       `(${TimestampCommand.UCLA_TIMEZONE}).`,
+    )
+    .addBooleanOption(input => input
+      .setName("help")
+      .setDescription("Display help about timestamps (ignore other options)."),
     )
     .addIntegerOption(input => input
       .setName("year")
@@ -114,6 +124,14 @@ class TimestampCommand extends SlashCommandHandler {
   public override async execute(
     interaction: ChatInputCommandInteraction,
   ): Promise<void> {
+    const help = interaction.options.getBoolean("help");
+
+    if (help) {
+      const embed = this.formatHelpEmbed();
+      await interaction.reply({ embeds: [embed], ephemeral: true });
+      return;
+    }
+
     const style = interaction.options.getString(
       "style",
     ) as TimestampStylesString | null;
@@ -164,6 +182,66 @@ class TimestampCommand extends SlashCommandHandler {
     }
 
     return dateTime;
+  }
+
+  private formatHelpEmbed(): EmbedBuilder {
+    const now = this.dateClient.getNow();
+
+    const example = `Example rendered mention: ${time(now)}`;
+
+    const commandDescription = (
+      `Format a ${bold("timestamp mention")}, which encodes a date/time that ` +
+      "automatically renders in every user's current timezone. You can also " +
+      `do neat things like using the ${italic("relative time")} format style ` +
+      "to render countdowns. This command in particular will treat input as " +
+      `UCLA time (${inlineCode(TimestampCommand.UCLA_TIMEZONE)}).`
+    );
+
+    const syntaxOverview = (
+      "The syntax of a timestamp mention is:\n" +
+      codeBlock("<t:UNIX_TIMESTAMP>") +
+      "Or with a format style code:\n" +
+      codeBlock("<t:UNIX_TIMESTAMP:STYLE>") +
+      "You can also generate Unix timestamps using " +
+      quietHyperlink(
+        "this online tool",
+        "https://www.unixtimestamp.com" as UrlString,
+      ) + "."
+    );
+
+    const formatStylesReference = bold("Format Styles Reference") + "\n" +
+      toBulletedList(STYLE_CHOICES.map(choice => {
+        const { name: displayName, value } = choice;
+        // Discard the (e.g. ...) from the choice display name, we'll replace
+        // it with an actual rendered timestamp.
+        const name = displayName.slice(0, displayName.indexOf("(") - 1);
+        const mention = time(now, value);
+        return (
+          `${inlineCode(value)}: ${name} (${inlineCode(mention)} => ${mention})`
+        );
+      }));
+
+    const optionsReference = bold("Options Reference") + "\n" + toBulletedList([
+      "If you omit any of the date/time units (year, month, second, etc.), " +
+      "they default to those of the current time.",
+
+      "By default, the raw timestamp text is returned ephemerally for your " +
+      `copy-paste. You can set ${inlineCode("render")} to directly render ` +
+      "(publicly) the timestamp instead.",
+    ]);
+
+    const combinedDescription = [
+      example,
+      commandDescription,
+      syntaxOverview,
+      formatStylesReference,
+      optionsReference,
+    ].join("\n\n");
+
+    return new EmbedBuilder()
+      .setColor(Colors.Green)
+      .setTitle(`${this.id} Help`)
+      .setDescription(combinedDescription);
   }
 }
 
