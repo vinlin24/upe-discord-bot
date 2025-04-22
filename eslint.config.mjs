a958216670460b3eb8b66a7230faeb7d3e8d7ad9
Vincent Lin <vinlin24@outlook.com>
2025-04-21 17:09:38 -0700

    Add minimal ESLint to ban process.env access

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import node from "eslint-plugin-node";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config({
+  files: [
+    "**/*.ts",
+  ],
+  languageOptions: {
+    parser: tseslint.parser,
+  },
+  plugins: {
+    node,
+  },
+  rules: {
+    // Disallow direct access to `process.env`. All environment variables should
+    // go through the validated env object exported on startup.
+    "node/no-process-env": "error",
+  },
+});
