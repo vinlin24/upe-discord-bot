2865585e4a846068042ecfe5d970b039d6127634
Tyler Xiao <yxiao1717@gmail.com>
2025-08-21 00:35:06 -0700

    Add prettier formatting scripts to codebase

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,26 @@
 import node from "eslint-plugin-node";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config({
-  files: [
-    "**/*.ts",
-  ],
-  languageOptions: {
-    parser: tseslint.parser,
+export default tseslint.config(
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: tseslint.parser,
+    },
+    plugins: {
+      node,
+    },
+    rules: {
+      // Disallow direct access to `process.env`. All environment variables should
+      // go through the validated env object exported on startup.
+      "node/no-process-env": "error",
+    },
   },
-  plugins: {
-    node,
+  {
+    files: ["**/*.{js,ts,mjs,cjs}"],
+    rules: {
+      "@typescript-eslint/indent": "on",
+      "@typescript-eslint/comma-dangle": "on",
+    },
   },
-  rules: {
-    // Disallow direct access to `process.env`. All environment variables should
-    // go through the validated env object exported on startup.
-    "node/no-process-env": "error",
-  },
-});
+);
