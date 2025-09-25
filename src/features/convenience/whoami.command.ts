8f03c4ef5826f3a4524487b44d01bc01ef34bf85
Vincent Lin <vinlin24@outlook.com>
2025-09-25 13:06:37 -0700

    Respond w/ user ID instead of username in /whoami

--- src/features/convenience/whoami.command.ts
+++ src/features/convenience/whoami.command.ts
@@ -1,7 +1,6 @@
 import {
-  codeBlock,
   SlashCommandBuilder,
-  type ChatInputCommandInteraction
+  type ChatInputCommandInteraction,
 } from "discord.js";
 
 import { SlashCommandHandler } from "../../abc/command.abc";
@@ -9,10 +8,10 @@ import { SlashCommandHandler } from "../../abc/command.abc";
 class WhoamiCommand extends SlashCommandHandler {
   public override readonly definition = new SlashCommandBuilder()
     .setName("whoami")
-    .setDescription("Check what your Discord username is.")
+    .setDescription("Get your Discord ID")
     .addUserOption(input => input
       .setName("user")
-      .setDescription("User whose username to check. Defaults to yourself.")
+      .setDescription("User whose ID to check. Defaults to yourself.")
     )
     .toJSON();
 
@@ -21,7 +20,7 @@ class WhoamiCommand extends SlashCommandHandler {
   ): Promise<void> {
     const targetUser = interaction.options.getUser("user") ?? interaction.user;
     await interaction.reply({
-      content: codeBlock(targetUser.username),
+      content: targetUser.id,
       ephemeral: true,
     });
   }
