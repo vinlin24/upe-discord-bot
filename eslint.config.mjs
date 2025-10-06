1e2c692055bc7b8e63d5d385ef54705b6890a656
Vincent Lin <vinlin24@outlook.com>
2025-10-06 12:28:26 -0700

    Revert "chore: add prettier formatting scripts to codebase (#22)"
    This reverts commit b9ae3efb32cd0e4140c0260bd1ac0ef3aa08038c.
    
    Faulty config. Fix at a later date. I need CI to work right now.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,26 +1,19 @@
 import node from "eslint-plugin-node";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(
-  {
-    files: ["**/*.ts"],
-    languageOptions: {
-      parser: tseslint.parser,
-    },
-    plugins: {
-      node,
-    },
-    rules: {
-      // Disallow direct access to `process.env`. All environment variables should
-      // go through the validated env object exported on startup.
-      "node/no-process-env": "error",
-    },
+export default tseslint.config({
+  files: [
+    "**/*.ts",
+  ],
+  languageOptions: {
+    parser: tseslint.parser,
   },
-  {
-    files: ["**/*.{js,ts,mjs,cjs}"],
-    rules: {
-      "@typescript-eslint/indent": "on",
-      "@typescript-eslint/comma-dangle": "on",
-    },
+  plugins: {
+    node,
   },
-);
+  rules: {
+    // Disallow direct access to `process.env`. All environment variables should
+    // go through the validated env object exported on startup.
+    "node/no-process-env": "error",
+  },
+});
